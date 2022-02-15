5c47b1632d2d70ce6406d6fd19a98f516c431523
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-02-15 04:31:13 -0800

    100. Same Tree

--- problem_solutions/same_tree.py
+++ problem_solutions/same_tree.py
@@ -5,18 +5,8 @@ Two binary trees are considered the same if they are structurally identical, and
 '''
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (p and not q) or (q and not p): return False
-        if (not p and not q): return True
-        if p.val == q.val:
-            if p.left and q.left and p.right and q.right:
-                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-            elif p.left and q.left and not p.right and not q.right:
-                return self.isSameTree(p.left, q.left)
-            elif not p.left and not q.left and p.right and q.right:
-                return self.isSameTree(p.right, q.right)
-            elif not p.left and not q.left and not p.right and not q.right:
-                return True
-            else:
-                return False       
-        else:
+        if not p and not q: 
+            return True
+        if (not p and q) or (not q and p) or p.val != q.val:
             return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
