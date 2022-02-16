7252e65919c27aa35e342e84e31444d1732f5826
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-02-16 14:33:58 -0800

    278. First Bad Version

---
+++ problem_solutions/first_bad_version.py
@@ -0,0 +1,17 @@
+'''
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+'''
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        start = 1
+        end = n
+        while end > start:
+            if isBadVersion(start + (end - start) // 2):
+                end = start + (end - start) // 2  
+            else:
+                start += (end - start) // 2 + 1
+        return end
