545eee5c3bd3a56e61db963e7d3bdc54222dec81
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-02-17 19:00:51 -0800

    595. Classes More Than 5 Students

---
+++ problem_solutions/classes_more_than_5_students.sql
@@ -0,0 +1,15 @@
+'''
+Write an SQL query to report all the classes that have at least five students.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+'''
+SELECT
+    class
+FROM 
+    Courses
+GROUP BY
+    class
+HAVING
+    COUNT(student) >= 5
