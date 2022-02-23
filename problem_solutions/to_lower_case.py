0fed537b7b8a99ab3ff3e031ffde6d02bf2e2932
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-02-23 15:47:00 -0800

    790. To Lower Case

---
+++ problem_solutions/to_lower_case.py
@@ -0,0 +1,6 @@
+'''
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+'''
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
