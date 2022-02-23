897304b6958326add9b14167c1892cb21a7e88be
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-02-22 19:48:59 -0800

    696. Binary Number with Alternating Bits

--- problem_solutions/binary_number_with_alternating_bits.py
+++ problem_solutions/binary_number_with_alternating_bits.py
@@ -3,10 +3,8 @@ Given a positive integer, check whether it has alternating bits: namely, if two 
 '''
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        ans= 0
-        ans_odd = set([])
-        for i in range(0, 34, 2):
-            ans += 2 ** i
-            ans_odd.add(ans)
-        ans_even = {ans >> 1 for ans in ans_odd}
-        return (n in ans_odd  or n in ans_even)       
+        while n:
+            if not (n ^ (n >> 1)) % 2:
+                return False
+            n >>= 1
+        return True    
