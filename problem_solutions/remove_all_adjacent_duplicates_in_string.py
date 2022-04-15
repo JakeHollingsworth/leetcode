3ec4edd08540f0905d27432f20ec02dcf285c56f
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-04-14 18:27:16 -0700

    1047. Remove All Adjacent Duplicates In String

---
+++ problem_solutions/remove_all_adjacent_duplicates_in_string.py
@@ -0,0 +1,16 @@
+'''
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+'''
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and stack[-1] == c:
+                stack.pop(-1)
+            else:
+                stack.append(c)
+        return "".join(stack)
