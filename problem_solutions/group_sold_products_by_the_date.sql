e24531a89f4059cc6fe65c77074ee8331794328a
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-07-27 13:17:38 -0700

    1484. Group Sold Products By The Date

---
+++ problem_solutions/group_sold_products_by_the_date.sql
@@ -0,0 +1,13 @@
+'''
+Write an SQL query to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+
+The query result format is in the following example.
+'''
+# Write your MySQL query statement below
+SELECT sell_date, COUNT(DISTINCT product) as num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product ASC) AS products
+FROM Activities
+GROUP BY sell_date
