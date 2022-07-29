f915f19b7602781e7f7a15045473003bdd7f6670
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-07-29 12:28:37 -0700

    1832. Check if the Sentence Is Pangram

---
+++ problem_solutions/check_if_the_sentence_is_pangram.py
@@ -0,0 +1,8 @@
+'''
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+'''
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        return len(set(sentence)) == 26
