8ae27315357c527b7925230f6454388d2516185f
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-07-29 12:15:48 -0700

    1816. Truncate Sequence

---
+++ problem_solutions/truncate_sentence.py
@@ -0,0 +1,8 @@
+'''
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        return ' '.join(s.split(' ')[:k])
+'''
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        return ' '.join(s.split(' ')[:k])
