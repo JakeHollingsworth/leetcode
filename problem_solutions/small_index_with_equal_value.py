0b9aed5dbdf92297724c0e395d0072d1d98826c5
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-11-13 11:55:13 -0800

    2057. Smallest Index With Equal Value

---
+++ problem_solutions/small_index_with_equal_value.py
@@ -0,0 +1,11 @@
+'''
+Given a 0-indexed integer array nums, return the smallest index i of nums such that i mod 10 == nums[i], or -1 if such index does not exist.
+
+x mod y denotes the remainder when x is divided by y.
+'''
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i, n in enumerate(nums):
+            if i % 10 == n:
+                return i
+        return -1
