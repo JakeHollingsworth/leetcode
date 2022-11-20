9bd144193a1041c5970806870a8a2be3a84a35e4
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-11-20 14:04:51 -0800

    2373. Largest Local Values in a Matrix

---
+++ problem_solutions/largest_local_values_in_a_matrix.py
@@ -0,0 +1,13 @@
+'''
+You are given an n x n integer matrix grid.
+
+Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+
+maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+
+Return the generated matrix.
+'''
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        return [[max(max(row[j-1:j+2]) for row in grid[i-1:i+2]) for j in range(1, len(grid)-1)] for i in range(1,len(grid)-1)]
