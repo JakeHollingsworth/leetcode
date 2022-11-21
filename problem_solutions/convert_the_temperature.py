134def2e50e2b6358a53bbd13025222406c45039
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-11-20 17:52:48 -0800

    2469. Convert the Temperature

---
+++ problem_solutions/convert_the_temperature.py
@@ -0,0 +1,15 @@
+'''
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+Note that:
+
+Kelvin = Celsius + 273.15
+Fahrenheit = Celsius * 1.80 + 32.00
+'''
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [273.15 + celsius, 1.8 * celsius + 32]
