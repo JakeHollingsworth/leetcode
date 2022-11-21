fd118afb71dfe9a2348148bba35993b9e8a63adb
JakeHollingsworth <jakehollingsworth@protonmail.com>
2022-11-20 17:49:15 -0800

    2460. Apply Operations to an Array

---
+++ problem_solutions/apply_operations_to_an_array.py
@@ -0,0 +1,21 @@
+'''
+You are given a 0-indexed array nums of size n consisting of non-negative integers.
+
+You need to apply n - 1 operations to this array where, in the ith operation (0-indexed), you will apply the following on the ith element of nums:
+
+If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
+After performing all the operations, shift all the 0's to the end of the array.
+
+For example, the array [1,0,2,0,0,1] after shifting all its 0's to the end, is [1,2,1,0,0,0].
+Return the resulting array.
+
+Note that the operations are applied sequentially, not all at once.
+'''
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+        res = [n for n in nums if n]
+        return res + [0] * (len(nums) - len(res))
