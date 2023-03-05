08ac5874048b9323b70341a05b91e13418ac909f
JakeHollingsworth <jakehollingsworth@protonmail.com>
2023-03-04 21:09:56 -0800

    6. Zigzag Conversion

---
+++ problem_solutions/medium/zigzag_conversion.py
@@ -0,0 +1,49 @@
+'''
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+ 
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+'''
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        if numRows == 2:
+            return s[::2] + s[1::2]
+        first_row = s[::2 * numRows - 2]
+        nth_row = [(s[n::2*numRows-2], s[2 * numRows - 2-n::2 * numRows - 2]) for n in range(1, numRows-1)]
+        nth_row = [''.join([a[i//2] if not i % 2 else b[i//2] for i in range(len(a+b))]) for a,b in nth_row]
+        last_row = s[numRows-1::2*numRows-2]
+        return first_row + ''.join(nth_row) + last_row
